73dc14df837721de895dae335a069c2b99de6ea3
EZEQUIEL LAKODEY <ela.webdeveloper@gmail.com>
2023-07-17 20:43:30 -0300

    implementing web socket with handlebars.initial structure done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { productsRouter } from './routes/productsRoutes.js';
 import { cartRouter } from './routes/cartRoutes.js';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 8080;
@@ -12,6 +16,13 @@ app.use('/products', productsRouter);
 app.use('/carts', cartRouter);
 
 // launching server
-app.listen(port, () => {
+const httpServer = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const socketServer = new Server(httpServer);
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + './views');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + './public'));
+app.use('/', viewsRouter);
