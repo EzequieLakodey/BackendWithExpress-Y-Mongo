a1b202ff56e70e2f4e254fea2f17d5259ee1a454
EZEQUIEL LAKODEY <ela.webdeveloper@gmail.com>
2023-09-25 21:45:35 -0300

    project restructured, core installed

---
+++ backend/src/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { config } from './config/config.js';
+import { __dirname } from './utils.js';
+import path from 'path';
+import { app, httpServer } from './servers.js';
+import { connectDB } from './config/dbConnection.js';
+import { sessionsRouter } from './routes/sessions.routes.js';
+import { viewsRouter } from './routes/views.routes.js';
+import { productsRouter } from './routes/products.routes.js';
+import { cartsRouter } from './routes/carts.routes.js';
+import passport from 'passport';
+import { initializePassport } from './config/passportConfig.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+/* MODULES */
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+const port = config.server.port;
+
+httpServer.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
+
+connectDB();
+
+const hbs = handlebars.create({ defaultLayout: null, extname: '.hbs' });
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/views'));
+
+initializePassport();
+app.use(passport.initialize());
+
+app.use(express.static(__dirname + '/public'));
+app.use(viewsRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.use('/api/sessions', sessionsRouter);
