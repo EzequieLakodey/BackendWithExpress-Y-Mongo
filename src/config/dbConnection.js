1cb62d7f35266463f9c761a9e670fd31fa7283f3
Ezequiel Lakodey <ela.webdeveloper@gmail.com>
2023-12-24 16:49:32 -0300

    minor log added

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -4,6 +4,7 @@ import { config } from './config.js';
 export const connectDB = async () => {
     try {
         await mongoose.connect(`${config.mongo.url}/ecommerce`);
+        console.log('listenning on port 8080');
     } catch (error) {
         `Error connecting to the DB: ${error.message}`;
     }
